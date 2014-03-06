0620a2a12cfbdeb15d9ab2142af5a5f52a0fbb52
Evaristo GZ <evgarciaz@gmail.com>
2014-03-06 20:46:28 +0100

    Eliminado código que hacía referencia a JSON

--- tiempo-xml.py
+++ tiempo-xml.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
-import json
 import requests
+from lxml import etree
 from jinja2 import Template
 import webbrowser
-print "Aplicación base con salida HTML || OpenWeatherMap"
+print "Aplicación base con salida HTML tras respuesta en XML || OpenWeatherMap"
 print ""
 
 def cardinal(direccion):
@@ -41,7 +41,6 @@ allpuntocardinal = []
 prov = ['Almeria', 'Cadiz', 'Cordoba', 'Huelva', 'Jaen', 'Malaga', 'Sevilla']
 for elemento in prov:
  	datos = requests.get('http://api.openweathermap.org/data/2.5/weather',params={'q':'%s,spain' % elemento})
- 	valores = json.loads(datos.text) # Carga los datos en un diccionario json #
  	temp_max = int(valores['main']['temp_max'] - 273)
  	temp_min = int(valores['main']['temp_min'] - 273)
  	velocidad = int(valores['wind']['speed']*1.609)
